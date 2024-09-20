8ead685ade24d716fdff34f14b61abe016542010
tobyhooper <99554339+tobyhooper@users.noreply.github.com>
2024-09-20 22:16:19 +0100

    update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,18 +8,58 @@ vim.cmd [[
   Plug 'ellisonleao/gruvbox.nvim'
   Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
   Plug 'williamboman/mason.nvim'
-  Plug 'williamboman/mason-lspconfig.nvim'
   Plug 'neovim/nvim-lspconfig'
-  Plug 'mfussenegger/nvim-lint'
-  Plug 'mhartington/formatter.nvim'
-  Plug 'ms-jpq/coq_nvim', {'branch': 'coq'}
-  Plug 'ms-jpq/coq.artifacts', {'branch': 'artifacts'}
+  Plug 'hrsh7th/nvim-cmp'
+  Plug 'hrsh7th/cmp-nvim-lsp'
+  Plug 'VonHeikemen/lsp-zero.nvim', {'branch': 'v4.x'}
 
   call plug#end()
 ]]
 
---keymaps
+--general setup
 vim.g.mapleader = " "
+
+vim.g.maplocalleader = ' '
+
+vim.g.have_nerd_font = false
+
+vim.opt.number = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.showmode = true
+
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+vim.opt.inccommand = 'split'
+
+vim.opt.cursorline = true
+
+--scrolloff
+vim.opt.scrolloff = 4
+
 
 --netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -72,10 +112,28 @@ vim.keymap.set("n", "<leader>ph", ui.toggle_quick_menu)
 --mason
 require("mason").setup()
 
---coq
--- Import COQ and enable it
-vim.g.coq_settings = { auto_start = 'shut-up' } -- Auto-start COQ without manual intervention
-local coq = require("coq")
+--lsp-zero
+local lsp_zero = require('lsp-zero')
 
--- Set up LSP with COQ
-local lspconfig = require'lspconfig'
+-- lsp_attach is where you enable features that only work
+-- if there is a language server active in the file
+local lsp_attach = function(client, bufnr)
+  local opts = {buffer = bufnr}
+
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
+
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+})
